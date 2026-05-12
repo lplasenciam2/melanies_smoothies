7a476c8ff81ec00c85e7a1bee32a28d92de82f2f
lplasenciam2 <lplasenciam2@gmail.com>
2026-05-12 14:53:36 -0600

    Update streamlit_app.py
    Use search_on for lookup nutrition data

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,13 +46,13 @@ if ingredients_list:
       
       #New section to display smoothiefroot nutrition information
         st.subheader(fruit_chosen + " Nutrition Information")
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
       #New section to display smoothiefroot nutrition information  
     
     
     
-      
+
      
   
    # st.write(ingredients_string)
