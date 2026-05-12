1d5ddeda6e3078c4d4e709a104f3b3e264e2763f
lplasenciam2 <lplasenciam2@gmail.com>
2026-05-12 13:54:04 -0600

    Update streamlit_app.py
    Updated code to display de Nutrition info for each fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen + ' '
       #New section to display smoothiefroot nutrition information
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
       #New section to display smoothiefroot nutrition information  
     
