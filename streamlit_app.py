1921c2c159665659bef7d9445f6dd9988778d033
lplasenciam2 <lplasenciam2@gmail.com>
2026-05-12 14:43:18 -0600

    Update streamlit_app.py
    STEP BY STEP FIX PANDAS IMPORT FEATURES

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit
 
 #Convert the SNOWPARK DF to a PANDAS DF so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list =  st.multiselect(
     'Choose up to 5 ingredients: '
@@ -40,8 +40,10 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen + ' '
-        search_on= pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH ON'].iloc[0]
+      
+        search_on= pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'search_on'].iloc[0]
         st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+      
       #New section to display smoothiefroot nutrition information
         st.subheader(fruit_chosen + " Nutrition Information")
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
